2901ce953e3d8ff18e9fc91f6569802eca4c509b
RyotaNakata <ryota.n28060952@gmail.com>
2025-08-17 09:04:31 +0900

    fix: JavaScriptファイル切断問題を修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,38 @@
+import path from "path";
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
-import path from "path";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
+  plugins: [RubyPlugin()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3036,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app/frontend"),
     },
-    extensions: ['.js', '.ts', '.tsx', '.jsx'],
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
   },
   css: {
     modules: {
       localsConvention: "camelCase",
       generateScopedName: "style_[local]__[hash:base64:5]",
     },
+  },
+  build: {
+    manifest: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+    target: "es2019",
+  },
+  esbuild: {
+    target: "safari13",
   },
 });
